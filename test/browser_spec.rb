41b3bf25e8b63d558ba0f455b65b3ade87d5d531
Joost Altena <jealt13@gmail.com>
2014-10-08 12:01:00 +0200

    Add desktop? tests

--- test/browser_spec.rb
+++ test/browser_spec.rb
@@ -62,6 +62,7 @@ describe Browser do
     assert @browser.safari?
     assert @browser.webkit?
     assert @browser.modern?
+    assert @browser.desktop?
     assert_equal "5.0.1", @browser.full_version
     assert_equal "5", @browser.version
   end
@@ -78,6 +79,7 @@ describe Browser do
     assert @browser.ios?
     refute @browser.tablet?
     refute @browser.mac?
+    refute @browser.desktop?
     assert_equal "3.0", @browser.full_version
     assert_equal "3", @browser.version
   end
@@ -94,6 +96,7 @@ describe Browser do
     assert @browser.tablet?
     refute @browser.mobile?
     refute @browser.mac?
+    refute @browser.desktop?
     assert_equal "4.0.4", @browser.full_version
     assert_equal "4", @browser.version
   end
